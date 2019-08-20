2f9ad6fd63fd6cdb34b6496f988c2a324a3b7686
Chen Wang <wangcees@gmail.com>
2019-08-20 14:17:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,9 @@ def reverse_each_word(sentence)
 array = sentence.split(" ")
 answer = ""
 index = 0
-  array.collect! do |word| 
+  array.collect do |word| 
     array[index] = word.reverse
-    index +=1
+    index += 1
   end
   array.join(" ")
 end
